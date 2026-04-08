2f712d309d01a1ed84d15f149bb69b5eb52956b9
Sourav Kumar <souravkumar66262@gmail.com>
2026-04-08 18:00:44 +0600

    Added frontend rate limiting logic to Contact page

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+
+app.set('trust proxy', 1);
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const contactLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 3, // Limit each IP to 3 requests
+  message: { error: "You are sending messages too fast! Please try again after 15 minutes." }
+});
 
 // Routes Import
 const projectRoutes = require('./routes/projectRoutes');
@@ -16,7 +28,7 @@ const contactRoutes = require('./routes/contactRoutes');
 
 // Use Routes
 app.use('/api/projects', projectRoutes);
-app.use('/api/contact', contactRoutes);
+app.use('/api/contact', contactLimiter, contactRoutes);
 
 // Basic Route for Testing
 app.get('/', (req, res) => {
